900e2e5d48a5de0dd0c25dfb0ad342d8aa0a1567
Noor2610 <nf1918939@gmail.com>
2026-06-12 15:00:15 +0500

    Delete week 7 directory

--- week 7/task 2.py
+++
@@ -1,9 +0,0 @@
-s={}
-a=type(s)
-print(a)
-print(s)
-
-
-n=set()
-type(n)
-print(n)
--- week 7/task 3.py
+++
@@ -1,7 +0,0 @@
-n={"maham","hifza","fiza","evan"}
-for i in n:
-    m=input("enter your name:")
-    if m in n:
-        print(" your name exists")
-    else:
-        print("not exist")
